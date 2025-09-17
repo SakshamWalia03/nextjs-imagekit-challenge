50f34257a0e1bdb45eb4244af01a0408870d97eb
Saksham Walia <145364995+SakshamWalia03@users.noreply.github.com>
2025-09-17 19:31:11 +0530

    feat(features): added features

--- src/components/studio/ai-magic-panel.tsx
+++ src/components/studio/ai-magic-panel.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 import {Brain, Crop, Palette, PenLine, Zap} from "lucide-react";
 
 import {
--- src/components/studio/audio-panel.tsx
+++ src/components/studio/audio-panel.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 import {Palette} from "lucide-react";
 
 import {
